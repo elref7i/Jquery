6985d6d8ab8b2c242935bc84dc5273a777c0ac00
Ahmed Refai <momo3956248@gmail.com>
2024-10-19 20:56:02 +0300

    Hours

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ $('.sidebar-link').on('click', function () {
   console.log($(this));
   console.log(Topsection);
 });
-
+/* 
 function updateDateClock() {
   const eventDay = new Date('2024-10-25T00:00:00');
   const now = new Date();
@@ -38,7 +38,37 @@ function updateDateClock() {
 }
 
 updateDateClock();
-setInterval(updateDateClock, 1000);
+setInterval(updateDateClock, 1000); */
+
+window.onload = function () {
+  countDownToTime('11 october 2025 9:56:00');
+};
+
+function countDownToTime(countTo) {
+  let futureDate = new Date(countTo);
+  futureDate = futureDate.getTime() / 1000;
+
+  let now = new Date();
+  now = now.getTime() / 1000;
+
+  timeDifference = futureDate - now;
+
+  let days = Math.floor(timeDifference / (24 * 60 * 60));
+  let daysToSec = days * (24 * 60 * 60);
+
+  let hours = Math.floor((timeDifference - daysToSec) / 3600);
+  let mins = Math.floor((timeDifference - daysToSec - hours * 3600) / 60);
+  let secs = Math.floor(timeDifference - daysToSec - hours * 3600 - mins * 60);
+
+  $('.day').text(`${days}`);
+  $('.hours').text(`${hours}`);
+  $('.minutes').text(`${mins}`);
+  $('.seconds').text(`${secs}`);
+
+  setInterval(function () {
+    countDownToTime(countTo);
+  }, 1000);
+}
 
 const maxMessage = 100;
 function checkLengthMessage() {
